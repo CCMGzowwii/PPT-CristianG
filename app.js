0dc83684ee0a4d68595eb4a2f7c9a8e911bb7461
zowwii <cristiancmgallego@gmail.com>
2024-07-18 19:22:31 -0500

    scrips

--- app.js
+++ app.js
@@ -7,7 +7,20 @@ const btnPaper = document.getElementById('paper')
 const btnScissor = document.getElementById('scissor')
 
 btnRock.addEventListener('click', chooseRock )
+btnPaper.addEventListener('click', choosePaper )
+btnScissor.addEventListener('click', chooseScissor )
 
 function chooseRock (){
-    document.getElementById('shift-user')
+    document.getElementById('shift-user').textContent = 'usuario eligio piedra'
+    return rock
+}
+
+function choosePaper (){
+    document.getElementById('shift-user').textContent = 'usuario eligio papel'
+    return paper
+}
+
+function chooseScissor (){
+    document.getElementById('shift-user').textContent = 'usuario eligio tijeras'
+    return scissor
 }
